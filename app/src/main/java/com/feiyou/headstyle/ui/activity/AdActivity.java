9121a3b2f19ecc52c6b39e8a6dd0d1a60cd2789f
myflying <512710257@qq.com>
2019-03-18 18:36:36 +0800

    fix bugs

---
+++ app/src/main/java/com/feiyou/headstyle/ui/activity/AdActivity.java
@@ -0,0 +1,88 @@
+package com.feiyou.headstyle.ui.activity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.blankj.utilcode.util.SizeUtils;
+import com.feiyou.headstyle.R;
+import com.feiyou.headstyle.ui.adapter.BlackListAdapter;
+import com.feiyou.headstyle.ui.base.BaseFragmentActivity;
+import com.feiyou.headstyle.ui.custom.VersionUpdateDialog;
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
+import com.qmuiteam.qmui.widget.QMUITopBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * Created by myflying on 2018/11/23.
+ */
+public class BlackListActivity extends BaseFragmentActivity {
+
+    @BindView(R.id.topbar)
+    QMUITopBar mTopBar;
+
+    ImageView mBackImageView;
+
+    @BindView(R.id.black_list)
+    RecyclerView mBlackListView;
+
+    BlackListAdapter blackListAdapter;
+
+    @Override
+    protected int getContextViewId() {
+        return R.layout.activity_black_list;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initTopBar();
+        initData();
+    }
+
+    private void initTopBar() {
+        QMUIStatusBarHelper.setStatusBarLightMode(this);
+        mTopBar.setTitle(getResources().getString(R.string.app_name));
+        View topSearchView = getLayoutInflater().inflate(R.layout.common_top_back, null);
+        topSearchView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, SizeUtils.dp2px(48)));
+        TextView titleTv = topSearchView.findViewById(R.id.tv_title);
+        titleTv.setText("黑名单");
+
+        mTopBar.setCenterView(topSearchView);
+        mBackImageView = topSearchView.findViewById(R.id.iv_back);
+        mBackImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popBackStack();
+            }
+        });
+    }
+
+    public void initData() {
+        List<String> list = new ArrayList<>();
+        list.add("test");
+        list.add("test");
+        list.add("test");
+        list.add("test");
+
+        blackListAdapter = new BlackListAdapter(this,list);
+        mBlackListView.setLayoutManager(new LinearLayoutManager(this));
+        mBlackListView.setAdapter(blackListAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        popBackStack();
+    }
+}
