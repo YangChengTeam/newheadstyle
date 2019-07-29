c07af2047c7664d3d8adbc24bd27ec7fe7fea742
myflying <512710257@qq.com>
2019-07-29 17:54:53 +0800

    1.修复首页的gif图

--- app/src/main/java/com/feiyou/headstyle/ui/activity/Main1Activity.java
+++ app/src/main/java/com/feiyou/headstyle/ui/activity/Main1Activity.java
@@ -171,7 +171,7 @@ public class Main1Activity extends BaseFragmentActivity implements VersionView, 
         //step2:(可选，强烈建议在合适的时机调用):申请部分权限，如read_phone_state,防止获取不了imei时候，下载类广告没有填充的问题。
         TTAdManagerHolder.get().requestPermissionIfNecessary(this);
 
-        //Glide.with(this).load(R.drawable.welfare_gif).into(mCreateIv);
+        Glide.with(this).load(R.drawable.welfare_gif).into(mCreateIv);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             currentIndex = bundle.getInt("home_index", 0);
